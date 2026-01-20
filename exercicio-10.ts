3363486d70687430ee545d711b4bed9021af5b31
Felipe Mendes Alves <felipemendes5552@gmail.com>
2026-01-20 10:30:49 -0300

    Add classes for Pessoa, Livro, and Emprestimo

---
+++ exercicio-10.ts
@@ -0,0 +1,24 @@
+class Pessoa {
+  constructor(nome) {
+    this.nome = nome
+  }
+}
+
+class Livro {
+  constructor(titulo) {
+    this.titulo = titulo
+    this.disponivel = true
+  }
+}
+
+class Emprestimo {
+  constructor(livro, pessoa) {
+    if (!livro.disponivel) {
+      console.log("Livro indisponível")
+      return
+    }
+    this.livro = livro
+    this.pessoa = pessoa
+    livro.disponivel = false
+  }
+}
